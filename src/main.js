725d580beae059e2f9fa8636e30665a5c7d98c02
Arm Band <bre4tn.let@gmail.com>
2019-01-26 13:00:07 +0900

    各種調整
    - IEBuster追加
    - Google Search Consoleファイル追加
    - Googleアナリティクス追加
    - ファイル整理
    - エラーページ制御テスト

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import bowser from '../node_modules/bowser/src/bowser'
+import VueAnalytics from 'vue-analytics'
 
 Vue.config.productionTip = false
+Vue.use(VueAnalytics, {
+    id: 'UA-132392783-1',
+    router
+})
 
 new Vue({
     router,
